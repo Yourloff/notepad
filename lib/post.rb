919189915a4490aec9f286d633d92b4fd4d59216
yourloff <alex.yourloff@gmail.com>
2022-03-30 14:59:27 +0700

    new methods: find_by_id and find_all

--- lib/post.rb
+++ lib/post.rb
@@ -1,102 +1,86 @@
+# encoding: utf-8
+
 require 'sqlite3'
 
+# Базовый класс «Запись» — здесь мы определим основные методы и свойства,
+# общие для всех типов записей.
 class Post
-  @@SQLITE_DB_FILE = File.join(__dir__, 'notepad.db')
+  SQLITE_DB_FILE = File.join(__dir__, 'notepad.db')
 
   def self.post_types
-    { 'Memo' => Memo, 'Task' => Task, 'Link' => Link }
+    {'Memo' => Memo, 'Task' => Task, 'Link' => Link}
   end
 
   def self.create(type)
     post_types[type].new
   end
 
-  def self.find(limit, type, id)
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
-
-    # 1. конкретная запись
-    if !id.nil?
-      db.results_as_hash = true
-
-      result = db.execute("SELECT * FROM posts WHERE rowid = ?", id)
-
-      result = result[0] if result.is_a? Array
-
-      db.close
-
-      if result.empty?
-        puts "Такой id #{id} не найден в базе"
-        return nil
-      else
-        post = create(result['type'])
-
-        post.load_data(result)
-
-        return post
-      end
-    else
-      # 2. вернуть таблицу записей
-      db.results_as_hash = false
-
-      query = 'SELECT rowid, * FROM posts '
-
-      query += 'WHERE type = :type ' unless type.nil?
-      query += 'ORDER by rowid DESC '
-      query += 'LIMIT :limit ' unless limit.nil?
-
-      statement = db.prepare(query)
-
-      statement.bind_param('type', type) unless type.nil?
-      statement.bind_param('limit', limit) unless limit.nil?
-
-      result = statement.execute!
-
-      statement.close
-      db.close
-
-      result
-    end
-  end
-
   def initialize
     @created_at = Time.now
-    @text = nil
+    @text = []
   end
 
-  def read_from_console; end
+  # Метод класса find_by_id находит в базе запись по идентификатору
+  def self.find_by_id(id)
+    # Библиотека "optparse" обработает ошибки, если искомый id был передан пользователем некорректно
+    # (OptionParser::MissingArgument,  OptionParser::InvalidOption)
+    # Добавим еще одну проверку, и если id не передали, мы ничего не ищем, а возвращаем nil
+    return if id.nil?
+
+    # Если id передали, едем дальше
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+    db.results_as_hash = true
+    result = db.execute('SELECT * FROM posts WHERE  rowid = ?', id)
+    db.close
+
+    # Если в результате запроса получили пустой массив, снова возвращаем nil
+    return nil if result.empty?
+
+    # Если результат не пуст, едем дальше
+    result = result[0]
+
+    # Создаем пост нужного типа, заполняем его данными и возвращаем
+    post = create(result['type'])
+    post.load_data(result)
+    post
+  end
+
+  # Метод класса find_all возвращает массив записей из базы данных, который
+  # можно например показать в виде таблицы на экране.
+  def self.find_all(limit, type)
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+
+    db.results_as_hash = false
+
+    query = 'SELECT rowid, * FROM posts '
+    query += 'WHERE type = :type ' unless type.nil?
+    query += 'ORDER by rowid DESC '
+    query += 'LIMIT :limit ' unless limit.nil?
+
+    statement = db.prepare query
+
+    statement.bind_param('type', type) unless type.nil?
+    statement.bind_param('limit', limit) unless limit.nil?
+
+    result = statement.execute!
+
+    statement.close
+    db.close
+
+    result
+  end
+
+  def read_from_console
+    # Этот метод должен быть реализован у каждого ребенка
+  end
 
   def to_strings
-    # todo
+    # Этот метод должен быть реализован у каждого ребенка
   end
 
-  def save
-    file = File.new(file_path, 'w:UTF-8')
-
-    to_strings.each { |item| file.puts(item) }
-
-    file.close
-  end
-
-  def file_path
-    current_path = File.dirname(__FILE__)
-
-    file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d_%H-%M-%S.txt")
-
-    "#{current_path}/#{file_name}"
-  end
-
-  def save_to_db
-    db = SQLite3::Database.open(@@SQLITE_DB_FILE)
-    db.results_as_hash = true
-
-    db.execute(
-      "INSERT INTO posts(#{to_db_hash.keys.join(',')}) VALUES (#{('?,' * to_db_hash.keys.size).chomp(',')})",
-      to_db_hash.values
-    )
-
-    insert_row_id = db.last_insert_row_id
-    db.close
-    insert_row_id
+  def load_data(data_hash)
+    @created_at = Time.parse(data_hash['created_at'])
+    @text = data_hash['text']
   end
 
   def to_db_hash
@@ -106,7 +90,37 @@ class Post
     }
   end
 
-  def load_data(data_hash)
-    @created_at = Time.parse(data_hash['created_at'])
+  def save_to_db
+    db = SQLite3::Database.open(SQLITE_DB_FILE)
+    db.results_as_hash = true
+
+    post_hash = to_db_hash
+
+    db.execute(
+      'INSERT INTO posts (' +
+        post_hash.keys.join(', ') +
+        ") VALUES (#{('?,' * post_hash.size).chomp(',')})",
+      post_hash.values
+    )
+
+    insert_row_id = db.last_insert_row_id
+    db.close
+    insert_row_id
+  end
+
+  def save
+    file = File.new(file_path, 'w:UTF-8')
+
+    to_strings.each { |string| file.puts(string) }
+
+    file.close
+  end
+
+  def file_path
+    current_path = File.dirname(__FILE__)
+
+    file_time = @created_at.strftime('%Y-%m-%d_%H-%M-%S')
+
+    "#{current_path}/#{self.class.name}_#{file_time}.txt"
   end
 end
--- read.rb
+++ read.rb
@@ -7,39 +7,35 @@ require 'optparse'
 
 options = {}
 
-# Заведем нужные нам опции
 OptionParser.new do |opt|
-  # Этот текст будет выводиться, когда программа запущена с опцией -h
   opt.banner = 'Usage: read.rb [options]'
 
-  # Вывод в случае, если запросили help
   opt.on('-h', 'Prints this help') do
     puts opt
     exit
   end
 
-  # Опция --type будет передавать тип поста, который мы хотим считать
   opt.on('--type POST_TYPE', 'какой тип постов показывать ' \
          '(по умолчанию любой)') { |o| options[:type] = o }
-
-  # Опция --id передает номер записи в базе данных (идентификатор)
   opt.on('--id POST_ID', 'если задан id — показываем подробно ' \
          ' только этот пост') { |o| options[:id] = o }
-
-  # Опция --limit передает, сколько записей мы хотим прочитать из базы
   opt.on('--limit NUMBER', 'сколько последних постов показать ' \
          '(по умолчанию все)') { |o| options[:limit] = o }
-
 end.parse!
 
-result = Post.find(options[:limit], options[:type], options[:id])
+result = if options[:id].nil?
+           # Если id не передали, ищем все записи по параметрам
+           Post.find_all(options[:limit], options[:type])
+         else
+           # Если передали — забиваем на остальные параметры и ищем по id
+           Post.find_by_id(options[:id])
+         end
 
 if result.is_a? Post
   puts "Запись #{result.class.name}, id = #{options[:id]}"
 
   result.to_strings.each { |line| puts line }
 else
-
   print '| id                 '
   print '| @type              '
   print '| @created_at        '
@@ -48,14 +44,13 @@ else
   print '| @due_date          '
   print '|'
 
+  # Используем safe навигацию, учитывая возможное значение nil для result
   result&.each do |row|
     puts
 
     row.each do |element|
       element_text = "| #{element.to_s.delete("\n")[0..17]}"
-
       element_text << ' ' * (21 - element_text.size)
-
       print element_text
     end
 
